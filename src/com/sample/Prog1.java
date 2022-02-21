907e38fa6bc7b9b02df2fea8e6de39f3859954d6
ThisPc <ThisPc@DESKTOP-SUG63ON>
2022-02-21 18:38:42 +0530

    "1st Change"

--- src/com/sample/Prog1.java
+++ src/com/sample/Prog1.java
@@ -1,5 +1,10 @@
 package com.sample;
 
-public class Prog1 {
-
+public class Prog1 
+{
+   public static void main(String[] args) 
+   {
+	   System.out.println("Hey");
+	   System.out.println("Hey");
+   }
 }
